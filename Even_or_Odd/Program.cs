ccbbf64259f56e052048dca9e4e895d4ced94ce9
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-04-02 15:00:23 +0530

    Add files via upload

---
+++ Even_or_Odd/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initializing the variable 'num'
+            int num;
+
+            //getting value from the user
+            Console.Write("Enter the number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            //condition to check even or odd
+            if (num%2==0)
+            {
+                Console.WriteLine("{0} is even number",num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is odd number",num);
+            }
+            Console.ReadKey();
+        }
+    }
+}
