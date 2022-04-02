0a40783e3e1d77d6a7975aa9a6a945841f46c89a
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-04-02 16:29:39 +0530

    Add files via upload

---
+++ Tables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int j=1; j<=10; j++)
+            {
+                Console.Write("{0} x {1} = {2} \n",n,j,n*j);
+            }
+        }
+    }
+}
