272f2650406f3e8183afa344a1a042b2ac7bc462
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-04-03 20:02:25 +0530

    Add files via upload

---
+++ Sum_of_elements_of_array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_Elements_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 8, 4, 9, 2, 10 };
+            Console.Write("Elements of array is : ");
+            foreach (int i in arr)
+            {
+                Console.Write("\t"+i);
+            }
+
+            int sum = arr.Sum();
+            Console.WriteLine("\nSum of elements of array : "+sum);
+
+            Console.ReadKey();
+        }
+    }
+}
