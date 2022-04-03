2653bce2eda7fcf79673e1e5f42f2dab0ce86aad
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-04-03 20:10:31 +0530

    Add files via upload

---
+++ Store_elements_in_array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store_elements_in_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int[] arr = new int[5];
+            Console.WriteLine("Enter the elements : ");
+            
+            for (i = 0; i < 5; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("The elements are : ");
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine("{0} ", arr[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
