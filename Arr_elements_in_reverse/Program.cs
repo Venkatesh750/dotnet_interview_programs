07f3df1d9511888f7aa38f89a5cdc97579f3ca46
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-04-03 20:30:29 +0530

    Add files via upload

---
+++ Arr_elements_in_reverse/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_the_elements_in_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int[] arr = new int[5];
+            Console.Write("Enter the elements : \n");
+            for (i=0; i<5; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("\nElements are\n");
+            for (i=0; i<5; i++)
+            {
+                Console.Write("{0} \t",arr[i]);
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("\nElements in reverse order\n");
+            Array.Reverse(arr);
+            for (i = 0; i < 5; i++)
+            {
+                Console.Write("{0} \t", arr[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
