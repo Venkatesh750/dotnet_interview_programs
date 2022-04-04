a9d684c4c2dbff9ef5256639fcc857a03c857e7c
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-04-04 11:36:50 +0530

    Add files via upload

---
+++ null_reference_exception/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace null_reference_exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string value = null;
+            try
+            {
+                int length = value.Length;
+                Console.WriteLine(length);
+
+                Console.ReadLine();
+            }
+            catch (NullReferenceException ne)
+            {
+                Console.WriteLine(ne.Message);
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
