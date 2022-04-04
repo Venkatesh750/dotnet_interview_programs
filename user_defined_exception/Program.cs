fe332915a97d2f1111ba2eb285dcbe55c628fda5
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-04-04 11:34:40 +0530

    Add files via upload

---
+++ user_defined_exception/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_defined_exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Welcome");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                if(a==5 || a < 5)
+                {
+                    Console.WriteLine("Congratulations you have got an {0}",a);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    throw (new OutofStockException("OutofStockExceptoin generated"));
+                    
+                }
+
+            }
+            catch (OutofStockException oe)
+            {
+                Console.WriteLine(oe.Message.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+    //Creating custom exception
+    public class OutofStockException : Exception
+    {
+        public OutofStockException(string message) : base(message)
+        {
+
+        }
+    }
+}
