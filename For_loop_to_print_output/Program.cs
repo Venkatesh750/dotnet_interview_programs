6c85c14beb8cbe45bc4ab736f890fd21e44ddbab
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-04-08 11:43:47 +0530

    Add files via upload

---
+++ For_loop_to_print_output/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Using for loop print the following output on console
+                 1
+                121
+               12321
+              1234321
+             123454321 */
+
+            int i, j;
+            for (i = 0; i < 6; i++)
+            {
+                for (j = 1; j <= 6 - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write("{0}", j);
+                }
+                for (j = i - 1; j >= 1; j--)
+                {
+                    Console.Write("{0}", j);
+                }
+                Console.WriteLine("\n");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
