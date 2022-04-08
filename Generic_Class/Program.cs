e55fafac99f9fc5e7c43ea4158c9a9acb4e380c8
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-04-08 11:09:42 +0530

    Add files via upload

---
+++ Generic_Class/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            GenericClass<int> i = new GenericClass<int>();
+            Console.WriteLine("Printing integer value");
+            i.GenericFn(5);
+            
+            GenericClass<string> s = new GenericClass<string>();
+            Console.WriteLine("Printing string value");
+            s.GenericFn("Venkatesh");
+
+            Console.ReadKey();
+        }
+    }
+
+    public class GenericClass<T>
+    {
+        public void GenericFn(T printValue)
+        {
+            Console.WriteLine(printValue);
+        }
+    }
+}
