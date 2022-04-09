fb63340e79dd415034fb2185673b9611378d6618
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-04-09 13:31:46 +0530

    Add files via upload

---
+++ Print_all_Substring/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter the value : ");
+                string val;
+                val = Console.ReadLine();
+                for (int i = 1; i < val.Length; i++)
+                {
+                    for (int j = 0; j <= val.Length - i; j++)
+                    {
+                        string substr = val.Substring(j, i);
+                        Console.WriteLine(substr);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
