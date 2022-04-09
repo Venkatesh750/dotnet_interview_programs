26add04c1f015f3163fcb309fd1afa39d9d45cb2
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-04-09 14:02:25 +0530

    Add files via upload

---
+++ Reverse_String_without_function/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rev_str_without_arr_fn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter the value : ");
+                string val;
+                val = Console.ReadLine();
+                Console.Write("String before reverse : {0}\n",val);
+
+                char[] ch = val.ToCharArray();
+
+                Console.Write("String after reverse : ");
+                string rev_string="";
+                int length;
+
+                length = val.Length - 1;
+
+                while (length >= 0)
+                {
+                    rev_string = rev_string + val[length];
+                    length--;
+                }
+                Console.WriteLine(rev_string);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
