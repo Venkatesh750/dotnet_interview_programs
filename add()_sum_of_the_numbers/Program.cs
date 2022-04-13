3012804f80e6cc26c459ccfd9ba773a6a692990d
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-04-13 11:19:57 +0530

    Update Program.cs

--- add()_sum_of_the_numbers/Program.cs
+++ add()_sum_of_the_numbers/Program.cs
@@ -19,7 +19,7 @@ namespace static_fn
             int num1, num2, res;
             Console.Write("Enter the first number : ");
             num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter the first number : ");
+            Console.Write("Enter the second number : ");
             num2 = Convert.ToInt32(Console.ReadLine());
 
             res = num1 + num2;
