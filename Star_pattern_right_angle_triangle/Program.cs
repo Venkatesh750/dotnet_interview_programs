d69f58e941da776f5f71147b1e6875463597b063
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-04-17 14:32:04 +0530

    Add files via upload

---
+++ Star_pattern_right_angle_triangle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Star_pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Write a program in C# Sharp to display the
+               pattern like right angle triangle using an asterisk. 
+                The pattern like :
+
+                *
+                **
+                ***
+                ****   */
+            int i, j;
+            for (i = 1; i <5; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
