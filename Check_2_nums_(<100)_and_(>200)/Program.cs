0cdc22ee0db091da7bf24113680862a0fd5b8443
Venkatesh750 <91948927+Venkatesh750@users.noreply.github.com>
2022-04-17 14:51:49 +0530

    Add files via upload

---
+++ Check_2_nums_(<100)_and_(>200)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lt_or_gt_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Write a C# program to check two given numbers where one is
+               less than 100 and other is greater than 200 */
+
+            int m, n;
+            Console.Write("Enter the first number (<100): ");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second number (>200): ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine((m < 100 && n > 200));
+
+            Console.ReadKey();
+        }
+    }
+}
